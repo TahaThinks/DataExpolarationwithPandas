3bc8bf55be09c14e8c933cab1046552b0499ebee
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-07-03 23:42:04 +0400

    Get Rows and Columns of the DataFrame

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import pandas as pd
 df = pd.read_csv('sample_data/salaries_by_college_major.csv')
 # print(df.head())
 
-# How many rows does the dataframe have?
+# How many rows & columns does the dataframe have?
 print(f"Number of Rows: {df.shape[0]}")
 print(f"Number of Columns: {df.shape[1]}")
