86b2cd25c73c3774e3e316523af53a628f5fca81
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-07-04 02:46:44 +0400

    Get Highest Starting Salary

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ print(f"Number of Rows: {df.shape[0]}")
 print(f"Number of Columns: {df.shape[1]}")
 
 # What are the labels for the columns? Do the columns have names?
-[print(column) for column in df.columns]
+# [print(column) for column in df.columns]
 
 # Are there any missing values in our dataframe? Does our dataframe contain any bad data?
-print(df.isna())
+# print(df.isna())
 
 # Create new Data Frame without Null Values
 clean_df = df.dropna()
+
+# Find College Major with Highest Starting Salaries?
+# Undergraduate Major
+# Starting Median Salary
+# Mid-Career Median Salary
+# Mid-Career 10th Percentile Salary
+# Mid-Career 90th Percentile Salary
+# Group
+highest_starting_salary = clean_df["Starting Median Salary"].max()
+print(f"Highest Starting Salary: {highest_starting_salary}")
